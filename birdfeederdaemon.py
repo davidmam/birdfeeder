0308e5067f76a41f955f33dc1edefc4661951dee
Dr David Martin <d.m..a.martin@dundee.ac.uk>
2017-01-25 20:01:06 +0000

    initial commit of local code

---
+++ birdfeederdaemon.py
@@ -0,0 +1,90 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan  3 19:44:16 2017
+
+Data logging for weighing birdfeeder
+
+@author: David
+"""
+
+
+# To kick off the script, run the following from the python directory:
+#   PYTHONPATH=`pwd` python testdaemon.py start
+
+#standard python libs
+import logging
+import time
+
+#third party libs
+#from daemon import runner
+from hx711 import HX711
+import RPi.GPIO as GPIO
+import sys
+import numpy as np  # sudo apt-get python-numpy
+from datetime import datetime
+from pymongo import MongoClient
+
+class App():
+    
+    def __init__(self):
+        self.stdin_path = '/dev/null'
+        self.stdout_path = '/dev/tty'
+        self.stderr_path = '/dev/tty'
+        self.pidfile_path =  '/var/run/testdaemon/testdaemon.pid'
+        self.pidfile_timeout = 5
+        self.feeder = HX711(5, 6)
+        self.feeder.set_scale(7.99)
+        self.feeder.set_reference_unit(92)
+        self.feeder.power_down()
+        self.feeder.power_up()
+        client = MongoClient()
+        self.db=client.test.birdfeeder
+        time.sleep(2)
+        self.feeder.tare()
+            
+    def run(self):
+        while True:
+            try:
+                weight = self.feeder.get_weight(5)
+                # save median value
+                timestamp = datetime.now()
+                self.db.insert({'Sensor': 'birdfeeder', 'timestamp': timestamp, 'weight': weight})
+                #self.feeder.power_down()
+                #self.feeder.power_up()
+                time.sleep(0.5)
+                
+            except (KeyboardInterrupt, SystemExit):
+                self.cleanAndExit()#Main code goes here ...
+            #Note that logger level needs to be set to logging.DEBUG before this shows up in the logs
+            '''logger.debug("Debug message")
+            logger.info("Info message")
+            logger.warn("Warning message")
+            logger.error("Error message")'''
+            
+
+
+    def cleanAndExit(self):
+        print( "Cleaning...")
+        GPIO.cleanup()
+        print ("Bye!")
+        sys.exit()
+    
+def runprog():
+    app = App()
+    '''
+    logger = logging.getLogger("DaemonLog")
+    logger.setLevel(logging.INFO)
+    formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+    handler = logging.FileHandler("/var/log/birdfeederdaemon/birdfeederdaemon.log")
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+    
+    daemon_runner = runner.DaemonRunner(app)
+    #This ensures that the logger file handle does not get closed during daemonization
+    daemon_runner.daemon_context.files_preserve=[handler.stream]
+    daemon_runner.do_action()
+    '''
+    app.run()
+
+if __name__ == '__main__':
+    runprog()
